40e4276926f1b4f505dee4d4775fbfaff3b559fe
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-12-12 13:54:58 +0800

    nalagyan ko na ng placeholder code for the buttons

--- game/gameplay.gd
+++ game/gameplay.gd
@@ -6,11 +6,37 @@ extends TextureButton
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_rock_pressed() -> void:
+	print('r') # Replace with function body.
 
 
-func _on_gameplay1_pressed() -> void:
-	pass
+func _on_scissors_pressed() -> void:
+	print('s') # Replace with function body.
+
+
+func _on_paper_pressed() -> void:
+	print('p') # Replace with function body.
+
+
+func _on_water_pressed() -> void:
+	print('w')# Replace with function body.
+
+
+func _on_wind_pressed() -> void:
+	print('a') # Replace with function body.
+
+
+func _on_fire_pressed() -> void:
+	print('f') # Replace with function body.
+
+
+func _on_electric_pressed() -> void:
+	print('i') # Replace with function body.
+
+
+func _on_devil_pressed() -> void:
+	print('e') # Replace with function body.
+
+
+func _on_dragon_pressed() -> void:
+	print('d') # Replace with function body.
