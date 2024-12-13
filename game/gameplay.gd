014ba6fee836be2f8d6f43140e4ee867ef22565c
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-12-13 12:27:39 +0800

    Done na sa it's a tie

--- game/gameplay.gd
+++ game/gameplay.gd
@@ -5,6 +5,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 @onready var arps = $Arps
+@onready var youWon = get_node("/root/Gameplay/youWon")
+@onready var youLose = get_node("/root/Gameplay/youLose")
+@onready var youTie = get_node("/root/Gameplay/youTie")
+@onready var gameplay_background = get_node("/root/Gameplay/bg1")
 
 func _on_rock_pressed(choice: String = "r") -> void:
 	arps.playerChoice = choice
@@ -97,9 +101,10 @@ func show_comp_animation(player, computer, winner_decision):
 	#if choose_winner == 'w':
 	#	you win screen
 
-
 func _on_play_again_pressed() -> void:
-	get_tree().change_scene_to_file("res://gameplay-animation.tscn")
+	youWon.set_visible(false) 
+	youLose.set_visible(false)
+	youTie.set_visible(false)
 
 
 func _on_leave_pressed() -> void:
